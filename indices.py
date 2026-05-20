7d32e1326318d183bd76a85d1e6bdcdc05b68c3d
Laura Monteiro <lauradm1@al.insper.edu.br>
2026-05-20 10:32:34 -0300

    indices

---
+++ indices.py
@@ -0,0 +1,25 @@
+#Tela
+WIDTH = 1440
+HEIGHT = 750
+FPS = 60
+
+game = True
+
+start_x = (WIDTH/2)-75
+start_y = (HEIGHT/2)-50
+size = 0.1 
+speed = 8
+barbieX = 10
+barbieY = -15
+
+
+puxasacolaconta = 1
+puxasacolascale = 1
+puxasacolaspeed = 40
+vidapuxasacola = 10000
+
+sacolaspeed = 2
+sacolascale = 0.35
+sacolapontos = 100
+
+pontos = 5000
