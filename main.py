4135acac5db68f74f14008a78e38419dd544cc6e
Joana <joanagf@al.insper.edu.br>
2026-05-20 10:56:56 -0300

    importando bibliotecas

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import pygame
 import math
 from indices import *
+from sys import exit
+import random
+import json
+import os
+
+pygame.init()
+
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption(‘Barbie shopping’)
+clock = pygame.time.Clock()
+
+
