56e0cc66eae1b7cc5997496ab4850e73a789a026
Mariana <marianatg1@al.insper.edu.br>
2026-05-21 11:39:19 -0300

    imagens

--- main.py
+++ main.py
@@ -12,4 +12,18 @@ window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption(‘Barbie shopping’)
 clock = pygame.time.Clock()
 
+background = pygame.image.load(r'jogo\imagensons\image\background0.png').convert_alpha()
+background = pygame.transform.scale(background, (WIDTH, HEIGHT))
+game_over_image = pygame.image.load(r'jogo\imagensons\image\game_over.png').convert_alpha()
+game_over_image = pygame.transform.scale(game_over_image, (WIDTH, HEIGHT))
+start_screen_image = pygame.image.load(r'jogo\imagensons\image\inicio.png').convert_alpha()
+start_screen_image = pygame.transform.scale(start_screen_image, (WIDTH, HEIGHT))
+start_button_image = pygame.image.load(r'jogo\imagensons\image\Start.png').convert_alpha()
+start_button_image = pygame.transform.scale(start_button_image, (200, 100))
+character_select_button_image = pygame.image.load(r'jogo\imagensons\image\character_select.png').convert_alpha()
+character_select_button_image = pygame.transform.rotozoom(character_select_button_image, 0, 1.2)
+character_select_button_image = pygame.transform.scale(character_select_button_image, (200, 100))
+ranking_button_image = pygame.image.load(r'jogo\imagensons\image\ranking.png').convert_alpha()
+ranking_button_image = pygame.transform.scale(ranking_button_image, (200, 100))
+
 
