1b598cc3452c92f9b3ccc5ccb6b39203fade6d65
Joana <joanagf@al.insper.edu.br>
2026-05-21 11:42:57 -0300

    sons

--- main.py
+++ main.py
@@ -26,4 +26,11 @@ character_select_button_image = pygame.transform.scale(character_select_button_i
 ranking_button_image = pygame.image.load(r'jogo\imagensons\image\ranking.png').convert_alpha()
 ranking_button_image = pygame.transform.scale(ranking_button_image, (200, 100))
 
+pygame.mixer.music.load(r'jogo\imagensons\sound\apocalypse.mp3')  
+pygame.mixer.music.set_volume(0.8)
+game_over_music = pygame.mixer.Sound(r'ogo\imagensons\sound\go_effect.mp3') 
+game_over_music.set_volume(0.5)
+shoot_sound = pygame.mixer.Sound(r'ogo\imagensons/sound\shot.mp3')
+shoot_sound.set_volume(0.1)
+music_loaded = True
 
